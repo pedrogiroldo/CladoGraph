1254b128728245152f599674f8e54ded6bbbc897
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-11-30 12:53:58 -0300

    save point - erro não foi corrigido

--- src/scripts/phylogeneticTreesScripts/generateNewick.ts
+++ src/scripts/phylogeneticTreesScripts/generateNewick.ts
@@ -16,74 +16,96 @@ type Props = {
 export default function generateNewick(props: Props) {
   const { traits, externalGroup, descendants } = props;
 
-  const traitsAndNumberOfDescendantsThatHaveThem = traits.map((trait) => {
-    const numberOfDescendants = descendants.reduce((count, descendant) => {
-      if (descendant.traitsIds.includes(trait.id)) {
-        return count + 1;
+  const filteredTraits = traits
+    .map((trait) => {
+      if (trait.active !== true) {
+        return null;
       }
-      return count;
-    }, 0);
 
-    // Criar um novo objeto com a propriedade 'descendants'
-    return {
-      ...trait,
-      descendants: numberOfDescendants,
-    };
-  });
+      return trait;
+    })
+    .filter((trait) => trait !== null);
 
-  const newDescendants = descendants.map((descendant) => {
-    // define value for syn, ples and apo for all descendant
-    descendant.synapomorphies = 0;
-    descendant.plesiomorphies = 0;
-    descendant.apomorphies = 0;
-
-    // search for plesiomorphies
-    descendant.traitsIds.forEach((traitId) => {
-      if (
-        externalGroup.traitsIds.includes(traitId) &&
-        (descendant.plesiomorphies || descendant.plesiomorphies === 0)
-      ) {
-        descendant.plesiomorphies += 1;
-      }
-    });
-
-    // serach for syn and apo
-    descendant.traitsIds.forEach((traitId) => {
-      traitsAndNumberOfDescendantsThatHaveThem.forEach((trait) => {
-        if (traitId === trait.id) {
-          if (
-            trait.descendants === 1 &&
-            (descendant.synapomorphies || descendant.synapomorphies === 0)
-          ) {
-            descendant.synapomorphies += 1;
-          }
-        } else if (
-          trait.descendants > 1 &&
-          (descendant.apomorphies || descendant.apomorphies === 0)
+  const traitsAndNumberOfDescendantsThatHaveThem = filteredTraits.map(
+    (trait) => {
+      const numberOfDescendants = descendants.reduce((count, descendant) => {
+        if (
+          // @ts-ignore
+          descendant.traitsIds.includes(trait.id) &&
+          descendant.active === true
         ) {
-          descendant.apomorphies += 1;
+          return count + 1;
+        }
+        return count;
+      }, 0);
+
+      // Criar um novo objeto com a propriedade 'descendants'
+      return {
+        ...trait,
+        descendants: numberOfDescendants,
+      };
+    },
+  );
+
+  const newDescendants = descendants
+    .map((descendant) => {
+      if (descendant.active !== true) {
+        return null;
+      }
+      // define value for syn, ples and apo for all descendant
+      descendant.synapomorphies = 0;
+      descendant.plesiomorphies = 0;
+      descendant.apomorphies = 0;
+
+      // search for plesiomorphies
+      descendant.traitsIds.forEach((traitId) => {
+        if (
+          externalGroup.traitsIds.includes(traitId) &&
+          (descendant.plesiomorphies || descendant.plesiomorphies === 0)
+        ) {
+          descendant.plesiomorphies += 1;
         }
       });
-    });
 
-    return descendant;
-  });
+      // serach for syn and apo
+      descendant.traitsIds.forEach((traitId) => {
+        traitsAndNumberOfDescendantsThatHaveThem.forEach((trait) => {
+          if (traitId === trait.id) {
+            if (
+              trait.descendants === 1 &&
+              (descendant.synapomorphies || descendant.synapomorphies === 0)
+            ) {
+              descendant.synapomorphies += 1;
+            }
+          } else if (
+            trait.descendants > 1 &&
+            (descendant.apomorphies || descendant.apomorphies === 0)
+          ) {
+            descendant.apomorphies += 1;
+          }
+        });
+      });
 
-  const compararDescendentes = (a: any, b: any) => {
+      return descendant;
+    })
+    .filter((descendant) => descendant !== null);
+
+  const compareDescendants = (a: any, b: any) => {
     if (a.synapomorphies !== b.synapomorphies) {
-      return b.synapomorphies - a.synapomorphies; // Ordena por Sin em ordem descendente
+      return a.synapomorphies - b.synapomorphies; // Ordena por Sin em ordem descendente
     } else if (a.plesiomorphies !== b.plesiomorphies) {
-      return a.plesiomorphies - b.plesiomorphies; // Ordena por Ples em ordem ascendente
+      return b.plesiomorphies - a.plesiomorphies; // Ordena por Ples em ordem ascendente
     } else {
-      return b.apomorphies - a.apomorphies; // Ordena por Apo em ordem descendente
+      return a.apomorphies - b.apomorphies; // Ordena por Apo em ordem descendente
     }
   };
 
-  const sortedDescendants = newDescendants.sort(compararDescendentes);
+  const sortedDescendants = newDescendants.sort(compareDescendants);
 
   // Construindo a string Newick
   const newick =
     sortedDescendants
+      // @ts-ignore
       .map(({ descendantName }) => `(${descendantName},`)
       .join('')
       .slice(0, -1) + Array(sortedDescendants.length + 1).join(')');
