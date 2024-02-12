f6b7cdfb3726142961d860bda08f33f97e45cb17
Pedro Giroldo <pedrogiroldo40@gmail.com>
2024-02-11 21:09:16 -0300

    inicio e alguns items ta página individual de templates

--- src/renderer/pages/templatesPage/Templates.tsx
+++ src/renderer/pages/templatesPage/Templates.tsx
@@ -1,11 +1,8 @@
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
 import Navbar from '../../components/Navbar/Navbar';
-import * as React from 'react';
 import TemplateItem from '../../components/TemplateItem/TemplateItem';
-import { Stack } from '@mui/material';
 import styles from './styles.module.css';
 
 export default function Templates() {
